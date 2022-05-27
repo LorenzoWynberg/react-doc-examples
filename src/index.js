6a3868ddd5ec986d45879f3e674ce07ee9b5fd57
LorenzoWynberg <lwcincr93@gmail.com>
2022-05-27 09:48:30 -0600

    Adding Local State to a Class

--- src/index.js
+++ src/index.js
@@ -1,63 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import App from './App';
+import Clock from './Clock';
 import reportWebVitals from './reportWebVitals';
-
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-
-function Avatar(props) {
-  return (
-    <img className="Avatar"
-         src={props.user.avatarUrl}
-         alt={props.user.name} />
-  );
-}
-
-function UserInfo(props) {
-  return (
-    <div className="UserInfo">
-      <Avatar user={props.user} />
-      <div className="UserInfo-name">
-        {props.user.name}
-      </div>
-    </div>
-  );
-}
-
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <UserInfo user={props.author} />
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text: 'I\'m Learning React!',
-  author: {
-    name: 'Sup Doggy',
-    avatarUrl: 'https://placedog.net/500'
-  }
-};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
 
 root.render(
-  <Comment
-    date={comment.date}
-    text={comment.text}
-    author={comment.author} />
+  <Clock />
 );
 
 // If you want to start measuring performance in your app, pass a function
