fa2b5a717546e479f9864a533e3dd9d64a3b16f4
LorenzoWynberg <lwcincr93@gmail.com>
2022-05-27 14:15:20 -0600

    List with keys

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Toggle from './Toggle';
+//import Clock from './Clock';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
 
+const numbers = [1,2,3,4,5]
+
+function ListItem(props) {
+  // Correct! There is no need to specify the key here:
+  return <li>{props.value}</li>;
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    // Correct! Key should be specified inside the array.
+    <ListItem key={number.toString()} value={number} />
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
+
 root.render(
-  <Toggle />
+  <NumberList numbers={numbers} />
 );
 
 // If you want to start measuring performance in your app, pass a function
