c31347d6ee465ce75237190bc92d4012a2836aba
LorenzoWynberg <lwcincr93@gmail.com>
2022-06-03 12:01:58 -0600

    Text Area

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import NameForm from './components/NameForm';
+import EssayForm from './components/EssayForm';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
 
-const numbers = [1,2,3,4,5];
-
 root.render(
-  <NameForm />
+  <EssayForm />
 );
 
 // If you want to start measuring performance in your app, pass a function
